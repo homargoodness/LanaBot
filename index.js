1ebd3eb73cb6258882d101e50a85b9b2325cfcfe
oma03 <omar.manir@sky.uk>
2018-11-15 21:57:26 +0000

    Added linting and config file.

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const Discord = require('discord.js');
+const config = require('/resrouces/config.js');
 const client = new Discord.Client();
 
-const games = [
-  'Supreme Commander: Forged Alliance',
-  'Supreme Commander 2',
-  'Killing Floor 2',
-  'Left for Dead 2'
-];
-
-console.log('bot running...');
-
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`bot running...Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", msg => {
   if(msg.content === "!whatGame") {
-    msg.reply(games[Math.floor(Math.random() * Math.floor(games.length))]);
-  };
+    msg.reply(config.games[Math.floor(Math.random() * Math.floor(config.games.length))]);
+  }
 });
 
 
 
-client.login("OAUTH2_TOKEN");
+client.login(config.token);
