3bc4ba1c99f187bd43cc9520e155865896f8ee3f
oma03 <omar.manir@sky.uk>
2018-12-20 22:08:36 +0000

    Typos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('/resrouces/config.js');
+const config = require('./resources/config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
