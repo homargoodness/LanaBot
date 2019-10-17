b0792fdbd657a8b3caa43dd3033e1992feef71dd
homargoodness <o.manir@gmail.com>
2019-10-17 22:25:02 +0100

    read config file using fs

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const Discord = require('discord.js');
-const config = require('./resources/config.json');
-const env = require('./resources/env.json');
+const { token } = require('./resources/env.json');
 const client = new Discord.Client();
+const fs = require('fs');
+
+let games = [];
+
+fs.readFile('./resources/config.json', (err, data) => {
+  if (err) console.log('Error reading config file');
+  games = JSON.parse(data).games;
+  console.log('Config file loaded');
+});
 
 client.on('ready', () => {
   console.log(`bot running...Logged in as ${client.user.tag}!`);
@@ -10,11 +18,11 @@ client.on('ready', () => {
 client.on("message", msg => {
   if (!msg.content.startsWith('!')) return;
 
-  if(msg.content === "!randomGame") {
-    msg.reply(config.games[Math.floor(Math.random() * Math.floor(config.games.length))]);
+  if(msg.content === "!game") {
+    msg.reply(games[Math.floor(Math.random() * Math.floor(games.length))]);
   }
 });
 
 
 
-client.login(env.token);
+client.login(token);
